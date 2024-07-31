bd7c8edea9f2f2c37278b37f38d36f5605e8d7e5
Y4ZK4Y <yk@yasamankarimi@gmail.com>
2024-07-31 11:56:09 +0200

    add time functions for getting current time and elapsed time

--- src/time.c
+++ src/time.c
@@ -1,2 +1,18 @@
 #include "philo.h"
 
+struct timeval	get_current_time(void)
+{
+	struct timeval	tv;
+	gettimeofday(&tv, NULL);
+	return (tv);
+}
+
+long	calculate_elapsed_time(struct timeval start, struct timeval end)
+{
+	long	seconds;
+	long	mseconds;
+	seconds = end.tv_sec - start.tv_sec;
+	mseconds = end.tv_usec - start.tv_usec;
+	return (seconds * 1000 + mseconds / 1000);
+
+}
