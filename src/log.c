2a2d4496e07833260aa2a640adf0e3ca1b2b0c5e
Y4ZK4Y <yk@yasamankarimi@gmail.com>
2024-08-01 11:52:03 +0200

    fix issue with timestamps

--- src/log.c
+++ src/log.c
@@ -6,16 +6,16 @@ void	log_message(t_philo *philo, msg_type msgtype)
 	struct timeval current_time;
 	const char *messages[NUM_MESSAGES] =
 	{
-		"%ld %d has taken a fork.",
-		"%ld %d is eating.",
-		"%ld %d is sleeping",
-		"%ld %d is thinking.",
-		"%ld %d died."
+		"timestamp:%ld, philo %d has taken a fork.\n",
+		"timestamp:%ld, philo %d is eating.\n",
+		"timestamp:%ld, philo %d is sleeping.\n",
+		"timestamp:%ld, philo %d is thinking.\n",
+		"timestamp:%ld, philo %d died.\n"
 	};
 
 	current_time = get_current_time();
     current_time_ms = calculate_elapsed_time(philo->info->start, current_time);	pthread_mutex_lock(&philo->info->write_lock); // error check
-	printf(messages[msgtype], current_time, philo->id);
+	printf(messages[msgtype], current_time_ms, philo->id);
 	pthread_mutex_unlock(&philo->info->write_lock);
 }
 
